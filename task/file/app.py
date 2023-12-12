33dc8fe884f97e0fd7ae0cf38ad4cacf147c3455
sikigasa <km3t1114@yahoo.co.jp>
2023-12-12 23:08:15 +0900

    add: task3

--- task/file/app.py
+++ task/file/app.py
@@ -7,6 +7,8 @@ from flask import (
     send_from_directory,
 )
 import os
+from os.path import join, relpath
+from glob import glob
 
 app = Flask(__name__)
 
@@ -20,6 +22,19 @@ def get_image_list():
     return images
 
 
+@app.route("/")
+def index():
+    path = "./static/img"
+    images = [relpath(x, path) for x in glob(join(path, "*"))]
+    my_list = []
+    for image in images:
+        my_dic = {}
+        my_dic["name"] = "static/img/"+image
+        my_list.append(my_dic)
+    return render_template("list.html", images = my_list)
+
+
+
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
     if request.method == "GET":
@@ -29,7 +44,9 @@ def upload():
         file = request.files["file"]
         file.save(os.path.join(UPLOAD_FOLDER, file.filename))
 
-        return redirect(url_for("uploaded_file", file_name=file.filename))
+        return redirect(url_for("index"))
+        #個別ページに飛ぶ
+        #return redirect(url_for("uploaded_file", file_name=file.filename))
 
 
 @app.route("/uploaded_file/<string:file_name>")
@@ -38,7 +55,7 @@ def uploaded_file(file_name):
 
     return render_template("uploaded_file.html", filename=file_path)
 
-
+# なくてもよい
 @app.route("/static/img/<string:file_name>")
 def send_img(file_name):
     return send_from_directory(UPLOAD_FOLDER, file_name)
--- task/file/upload.py
+++ task/file/upload.py
@@ -1,43 +1,45 @@
-from flask import (
-    Flask,
-    render_template,
-    request,
-    redirect,
-    url_for,
-    send_from_directory,
-)
-import os
+# これいらんけど、一応残しておく
 
-app = Flask(__name__)
+# from flask import (
+#     Flask,
+#     render_template,
+#     request,
+#     redirect,
+#     url_for,
+#     send_from_directory,
+# )
+# import os
 
-
-UPLOAD_FOLDER = "static/img"
-
-
-@app.route("/upload", methods=["GET", "POST"])
-def upload():
-    if request.method == "GET":
-        return render_template("upload.html")
-
-    elif request.method == "POST":
-        file = request.files["file"]
-
-        file.save(os.path.join(UPLOAD_FOLDER, file.filename))
-
-        return redirect(url_for("uploaded_file", file_name=file.filename))
+# app = Flask(__name__)
 
 
-@app.route("/uploaded_file/<string:file_name>")
-def uploaded_file(file_name):
-    file_path = os.path.join(UPLOAD_FOLDER, file_name)
-
-    return render_template("uploaded_file.html", filename=file_path)
+# UPLOAD_FOLDER = "static/img"
 
 
-@app.route("/static/img/<string:file_name>")
-def send_img(file_name):
-    return send_from_directory(UPLOAD_FOLDER, file_name)
+# @app.route("/upload", methods=["GET", "POST"])
+# def upload():
+#     if request.method == "GET":
+#         return render_template("upload.html")
+
+#     elif request.method == "POST":
+#         file = request.files["file"]
+
+#         file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+
+#         return redirect(url_for("uploaded_file", file_name=file.filename))
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+# @app.route("/uploaded_file/<string:file_name>")
+# def uploaded_file(file_name):
+#     file_path = os.path.join(UPLOAD_FOLDER, file_name)
+
+#     return render_template("uploaded_file.html", filename=file_path)
+
+# # なくてもよい
+# @app.route("/static/img/<string:file_name>")
+# def send_img(file_name):
+#     return send_from_directory(UPLOAD_FOLDER, file_name)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=8080)
