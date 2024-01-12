ce94bcf2c1d0bea622191f9e454a42ce3e6a0cf1
sikigasa <km3t1114@yahoo.co.jp>
2024-01-12 17:48:34 +0900

    update:授業

--- task/conda/translater/translator.py
+++ task/conda/translater/translator.py
@@ -1,32 +1,55 @@
 import requests, uuid, json
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-key = os.environ['key']
+key = os.environ["key"]
 endpoint = "https://api.cognitive.microsofttranslator.com/translate"
 location = "japaneast"
 
-constructed_url = endpoint
 
-params = {
-    "api-version": "3.0",
-    "from": "en",
-    "to": ["ja"]
-}
+app = Flask(__name__)
 
-headers = {
-    "Ocp-Apim-Subscription-Key": key,
-    "Ocp-Apim-Subscription-Region": location,
-    "Content-type": "application/json",
-    "X-ClientTraceId": str(uuid.uuid4),
-}
 
-body = [{'text': 'I would really like to drive your car around the block a few times.'}]
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-request = requests.post(constructed_url, params=params, headers=headers, json=body)
-print(request)
-response = request.json()
-print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
+
+@app.route("/translate", methods=["POST"])
+def translate():
+    constructed_url = endpoint
+
+    params = {
+        "api-version": "3.0",
+        "from": request.form.get["from_lang"],
+        "to": [request.form.get("to_lang")],
+    }
+
+    headers = {
+        "Ocp-Apim-Subscription-Key": key,
+        "Ocp-Apim-Subscription-Region": location,
+        "Content-type": "application/json",
+        "X-ClientTraceId": str(uuid.uuid4),
+    }
+
+    body = [
+        {"text": "I would really like to drive your car around the block a few times."}
+    ]
+
+    request = requests.post(constructed_url, params=params, headers=headers, json=body)
+    print(request)
+    response = request.json()
+    return render_template(
+        "translate.html",
+        res=json.dumps(
+            response,
+            sort_keys=True,
+            ensure_ascii=False,
+            indent=4,
+            separators=(",", ": "),
+        ),
+    )
