c4a96964f94063f07a000d60d35d98d051ca158b
sikigasa <km3t1114@yahoo.co.jp>
2024-01-23 17:29:22 +0900

    add: flask -translator

---
+++ class/flask-translator/translator/translator.py
@@ -0,0 +1,55 @@
+import requests, uuid, json
+from flask import Flask, render_template, request
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+key = os.environ["key"]
+endpoint = "https://api.cognitive.microsofttranslator.com/translate"
+location = "japaneast"
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/translate", methods=["POST"])
+def translate():
+    constructed_url = endpoint
+
+    params = {
+        "api-version": "3.0",
+        "from": request.form.get("from_lang"),
+        "to": request.form.get("to_lang"),
+    }
+
+    headers = {
+        "Ocp-Apim-Subscription-Key": key,
+        "Ocp-Apim-Subscription-Region": location,
+        "Content-type": "application/json",
+        "X-ClientTraceId": str(uuid.uuid4),
+    }
+
+    body = [
+        {"text": "I would really like to drive your car around the block a few times."}
+    ]
+
+    req = requests.post(constructed_url, params=params, headers=headers, json=body)
+    print(req)
+    response = req.json()
+    return render_template(
+        "translate.html",
+        res=json.dumps(
+            response,
+            sort_keys=True,
+            ensure_ascii=False,
+            indent=4,
+            separators=(",", ": "),
+        ),
+    )
