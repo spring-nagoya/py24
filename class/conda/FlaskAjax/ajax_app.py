d2bf7fd339b7afb4d83f1c64e967dd66f87e1293
sikigasa <km3t1114@yahoo.co.jp>
2024-02-13 16:42:07 +0900

    add:授業

--- class/conda/FlaskAjax/ajax_app.py
+++ class/conda/FlaskAjax/ajax_app.py
@@ -1,18 +1,21 @@
-from Flask import Flask, request, render_template
+from flask import Flask, request, render_template
 import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
-  
-@app.route("/call_ajax", methods=["GET","POST"])
+
+
+@app.route("/call_ajax", methods=["GET", "POST"])
 def call_ajax():
     if request.method == "POST":
         form = request.form["fdata"]
     if request.method == "GET":
         return "GET"
-      
+
+
 if __name__ == "__main__":
     app.run(debug=True)
