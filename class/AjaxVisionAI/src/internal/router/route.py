ec740f40e29f997395f7145124fe363f9cfb7806
murasame29 <oogiriminister@gmail.com>
2024-02-20 16:36:39 +0900

    add: templs

--- class/AjaxVisionAI/src/internal/router/route.py
+++ class/AjaxVisionAI/src/internal/router/route.py
@@ -10,11 +10,11 @@ DELETE= "DELETE"
 
 class Route:
     def __init__(self):
-        self.app = Flask(__name__,template_folder='../../templates')
+        self.app = Flask(__name__,template_folder='../../templates', static_folder='../../static')
         
         self.add_api("/",IndexHandler.as_view("index"))
         self.add_api("/health",HealthHandler.as_view("health"))
-        self.add_api("/call-ajax",AjaxHandler.as_view("ajax"))
+        self.add_api("/call_ajax",AjaxHandler.as_view("call-ajax"))
 
     def New(self):
         return self.app
